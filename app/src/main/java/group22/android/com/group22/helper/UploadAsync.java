ad7d64531d58523f1892f4a3ed4a1306f5c9be48
Amit Kumar <akuma152@asu.edu>
2017-03-08 22:28:15 -0700

    First commit

---
+++ app/src/main/java/group22/android/com/group22/helper/UploadAsync.java
@@ -0,0 +1,169 @@
+package com.example.dhire.mcassignment1;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+
+public class UploadAsync extends AsyncTask<Void, Void, Void> {
+
+    public MainActivity activity;
+    public UploadAsync(MainActivity a)
+    {
+        this.activity = a;
+    }
+    String upLoadServerUri = "https://impact.asu.edu/CSE535Fall16Folder/UploadToServer.php";
+    
+    int serverResponseCode = 0;
+    String srcFile;
+    ProgressDialog dialog ;
+
+    private void fileUpload() {
+
+    // This code is referred from http://www.coderefer.com/android-upload-file-to-server/
+        String fileName = srcFile;
+        //DBHelper dbHelper = new DBHelper(context);
+        HttpsURLConnection conn = null;
+        DataOutputStream dos = null;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        File sourceFile = new File(srcFile);
+        //if(bufferCounter>=maxCounter){
+
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            @Override
+            public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+                // Not implemented
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+                // Not implemented
+            }
+        } };
+
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        if (!sourceFile.isFile()) {
+            Log.e("uploadFile", "Source File does not exist :");
+
+        }
+        else
+        {
+            try {
+
+                // open a URL connection to the Servlet
+                FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                URL url = new URL(upLoadServerUri);
+                // Open a HTTP  connection to  the URL
+                conn = (HttpsURLConnection) url.openConnection();
+                conn.setDoInput(true); // Allow Inputs
+                conn.setDoOutput(true); // Allow Outputs
+                conn.setUseCaches(false); // Don't use a Cached Copy
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                conn.setRequestProperty("uploaded_file", fileName);
+                dos = new DataOutputStream(conn.getOutputStream());
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name=uploaded_file;"+"filename="
+                        + "HealthDB_Group3.db" + lineEnd);
+
+                dos.writeBytes(lineEnd);
+
+                // create a buffer of  maximum size
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                buffer = new byte[bufferSize];
+
+                // read file and write it into form...
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0) {
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                }
+
+                // send multipart form data necesssary after file data...
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                // Responses from the server (code and message)
+                serverResponseCode = conn.getResponseCode();
+                String serverResponseMessage = conn.getResponseMessage();
+
+                if(serverResponseCode == 200){
+                    Log.d("UploadService", "File Uploaded Successfully");
+                }
+
+                //close the streams //
+                fileInputStream.close();
+                dos.flush();
+                dos.close();
+
+            } catch (MalformedURLException ex) {
+                ex.printStackTrace();
+                Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.e("Upload file to serve", "Exception : "
+                        + e.getMessage(), e);
+            }
+        }
+    }
+
+    @Override
+    protected void onPreExecute() {
+        dialog = ProgressDialog.show(activity, "Uploading", "Please wait ...", true);
+    }
+    @Override
+    protected void onPostExecute(Void result) {
+        dialog.dismiss();
+    }
+    @Override
+    protected Void doInBackground(Void... params){
+
+        srcFile = "/data/user/0/com.example.dhire.mcassignment1/databases/HealthDB_Group3.db";
+
+        fileUpload();
+        return null;
+    }
+
+}
---
+++ app/src/main/java/group22/android/com/group22/model/DBTable.java
@@ -0,0 +1,8 @@
+package group22.android.com.group22.model;
+
+/**
+ * Created by amko0l on 3/7/2017.
+ */
+
+public class DBTAble {
+}
